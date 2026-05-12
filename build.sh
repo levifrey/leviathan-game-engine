f73f4c2df46f8c2733bd478f8fb8bc8d3476781d
Nimbusuar04 <levster2004@yahoo.com>
2026-05-12 00:15:32 -0700

    switching to other branch

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 rm -rf build
-<<<<<<< HEAD:make.sh
 cmake -S . -B build
 cmake --build build
-=======
->>>>>>> assimp:build.sh
 rm compile_commands.json
 cmake -S . -B build
 cmake --build build
